d8e4e6511a56cf2c85f2ac13aa41fef40ca26722
Daniel Winter <d.kreide@gmail.com>
2015-09-01 16:52:52 +0200

    latest paradeiser for zip file

--- gruntfile.js
+++ gruntfile.js
@@ -5,7 +5,6 @@ module.exports = function (grunt) {
         zip: {
             'using-cwd': {
                 cwd: 'bower_components/',
-                // Files will zip to 'hello.js' and 'world.js'
                 src: ['bower_components/Paradeiser/*/*', 'bower_components/Paradeiser/*/*/*'],
                 dest: 'paradeiser.zip'
             }
