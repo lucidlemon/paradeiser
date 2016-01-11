38df34d1bdb0c8dd7fab34bbce6599eb5454a06b
Daniel Winter <d.kreide@gmail.com>
2016-01-11 11:45:18 +0100

    optimizing the gulp file - now putting out the minified css on each gulp

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,36 @@ var merge = require('merge-stream');
 var sourcemaps = require('gulp-sourcemaps');
 var bump = require('gulp-bump');
 
+//react for demo
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var react = require('gulp-react');
+var htmlreplace = require('gulp-html-replace');
+
+var path = {
+    HTML: 'demo-src/index.html',
+    ALL: ['demo-src/js/*.js', 'demo-src/js/**/*.js', 'demo-src/index.html'],
+    JS: ['demo-src/js/*.js', 'demo-src/js/**/*.js'],
+    MINIFIED_OUT: 'build.min.js',
+    DEST_SRC: 'dist/demo-src',
+    DEST_BUILD: 'demo/build',
+    DEST: 'demo'
+};
+
+gulp.task('transform', function(){
+    gulp.src(path.JS)
+        .pipe(react())
+        .pipe(gulp.dest(path.DEST_SRC));
+});
+
+gulp.task('copy', function(){
+    gulp.src(path.HTML)
+        .pipe(gulp.dest(path.DEST));
+});
+
+gulp.task('watch', function(){
+    gulp.watch(path.ALL, ['transform', 'copy']);
+});
 
 gulp.task('bump', function(){
     gulp.src(['./bower.json', './package.json'])
@@ -23,23 +53,27 @@ gulp.task('bump-major', function(){
         .pipe(gulp.dest('./'));
 });
 
-gulp.task('default', function() {
-    var scss = gulp.src('./dev/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist'));
+gulp.task('compile-paradeiser', function(){
+    gulp.exec(['transform', 'copy']);
+});
 
-    var autoprefixed = gulp.src('dist/paradeiser.css')
+gulp.task('compile-paradeiser', function(){
+    var css = gulp.src('./dev/*.scss')
+        .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: ['last 5 versions', 'ie 8', 'ie 9'],
             cascade: false
-        }))
-        .pipe(gulp.dest('dist'));
+        }));
 
-    var min = autoprefixed.pipe(clone())
+    var min = css.pipe(clone())
         .pipe(cssnano({discardComments: {removeAll: true}}))
-        .pipe(rename('dist/min/paradeiser.min.css'));
+        .pipe(rename('min/paradeiser.min.css'));
 
-    return merge(autoprefixed, min)
+    return merge(css, min)
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(''));
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', function() {
+    gulp.start('compile-paradeiser');
 });
